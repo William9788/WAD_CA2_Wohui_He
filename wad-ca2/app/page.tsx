a3ce04f24d123a5971ce7a101790364a04f2bd60
William9788 <hewohui9788@icloud.com>
2026-01-10 22:58:03 +0000

    feat:code updated

---
+++ wad-ca2/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+  return(
+    <main className="mx-auto max-w-6xl rounded-[3rem] border-4 border-dotted border-red-500 bg-lime-200 px-8 pb-10 pt-6 shadow-xl">
+      <h1 className="text-2xl font-bold">
+        Learn to code with Code Club
+      </h1>
+    </main>
+  )
+}
