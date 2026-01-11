df263e0c6323927b4d89937a4a5b49e900a80e23
William9788 <hewohui9788@icloud.com>
2026-01-11 00:01:04 +0000

    feat:undo movement

---
+++ wad-ca2/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home(){
+  return(
+    <main className="mx-auto max-w-6xl rounded-[3rem] border-4 border-dotted border-red-500 bg-lime-200 px-8 pb-10 pt-6 shadow-xl">
+      <h1 className="text-2xl font-bold">
+        Learn to code with Code Club
+      </h1>
+      <p className="mt-4 text-lg leading-relaxed">
+        Our projects have step-by-step instructions to teach you how to create games,animations,and much more.
+      </p>
+      <p className="mt-2 text-lg leading-relaxed">
+        choose from hundreds of options, in up to 30 languages.
+      </p>
+    </main>
+  );
+}
